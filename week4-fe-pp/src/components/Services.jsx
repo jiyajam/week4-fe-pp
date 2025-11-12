b17b431f90f81188692df3dfe32f39a28df7be74
Hoang-Vuu <Hoang.Vu3@metropolia.fi>
2025-11-12 14:55:05 +0200

    Iteration 2

--- week4-fe-pp/src/components/Services.jsx
+++ week4-fe-pp/src/components/Services.jsx
@@ -1,8 +1,11 @@
 import { services } from '../data'
 import Title from './Title'
 import Service from './Service'
+import { useState } from 'react';
 
 const Services = () => {
+  const [servicesData, setServicesData] = useState(services);
+  
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
