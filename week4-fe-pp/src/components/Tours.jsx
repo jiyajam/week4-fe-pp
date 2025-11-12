6663e6b4f02dc99307d2bb2142717cae292cac51
Jiya <jiya.kanhirathanpoyil@metropolia.fi>
2025-11-12 15:02:56 +0200

    Iteration3

--- week4-fe-pp/src/components/Tours.jsx
+++ week4-fe-pp/src/components/Tours.jsx
@@ -1,18 +1,20 @@
-import { tours } from "../data";
-import Title from "./Title";
-import Tour from "./Tour";
+import { tours } from '../data'
+import Title from './Title'
+import Tour from './Tour'
+import { useState } from 'react'
 
 const Tours = () => {
+  const [toursData, setToursData] = useState(tours)
   return (
-    <section className="section" id="tours">
-      <Title title="featured" subTitle="tours" />
+    <section className='section' id='tours'>
+      <Title title='featured' subTitle='tours' />
 
-      <div className="section-center featured-center">
+      <div className='section-center featured-center'>
         {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
+          return <Tour key={tour.id} {...tour} />
         })}
       </div>
     </section>
-  );
-};
-export default Tours;
+  )
+}
+export default Tours
